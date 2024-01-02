42fe73331812df368a56e7776d40b0527499ccf6
Suado Cowboy <luccacsg@gmail.com>
2024-01-02 12:31:33 -0300

    game is not a class anymore

--- test/main.cpp
+++ test/main.cpp
@@ -2,31 +2,17 @@
 
 #include "engine.h"
 
-class Game
-{
-public:
-    Game(){}
-    ~Game(){}
-
-    void run()
-    {
-        Betoneira::Window window{500, 500, "Test game 2D"};
-
-        while (!window.shouldClose())
-        {
-            window.fill(0.2f, 0.3f, 0.3f);
-            
-            window.clear();
-        }
-    }
-};
-
 int main(int, char**)
 {
     Betoneira::init();
 
-    Game game{};
-    game.run();
+    Betoneira::Window window(800, 600, "2D Game Test");
+    while (!window.shouldClose())
+    {
+        window.fill(0.2f, 0.5f, 0.3f);
+
+        window.clear();
+    }
 
     Betoneira::quit(EXIT_SUCCESS); // already exits the process
 }
