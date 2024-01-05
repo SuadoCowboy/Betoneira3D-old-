e30fa34c8c2d53cf20a8cdfd0f1ed7201d96934d
Suado Cowboy <luccacsg@gmail.com>
2024-01-05 17:31:28 -0300

    added Colors enum and hexadecimal conversion function

--- engine/color/color.cpp
+++ engine/color/color.cpp
@@ -14,6 +14,11 @@ Color::Color(unsigned char _r, unsigned char _g, unsigned char _b, unsigned char
     updateGLColor();
 }
 
+Color::Color(int hexadecimal)
+{
+    setColor(Color::hexToColor(hexadecimal));
+}
+
 Color::~Color(){}
 
 unsigned char Color::getR()
@@ -36,7 +41,7 @@ unsigned char Color::getA()
     return a;
 }
 
-void Color::setRGBA(unsigned char _r, unsigned char _g, unsigned char _b, unsigned char _a)
+void Color::setColor(unsigned char _r, unsigned char _g, unsigned char _b, unsigned char _a)
 {
     r = _r;
     g = _g;
@@ -45,11 +50,26 @@ void Color::setRGBA(unsigned char _r, unsigned char _g, unsigned char _b, unsign
     updateGLColor();
 }
 
+void Color::setColor(Color newColor)
+{
+    r = newColor.getR();
+    g = newColor.getG();
+    b = newColor.getB();
+    a = newColor.getA();
+    updateGLColor();
+}
+
 GLfloat Color::byteToGLfloat(unsigned char c)
 {
     // FIMUL instruction
     // https://community.khronos.org/t/how-to-convert-from-rgb-255-to-opengl-float-color/29288/2
     return (1.0f/255) * c;
+}
+
+Color Color::hexToColor(int hexadecimalValue)
+{
+    // https://stackoverflow.com/questions/3723846/convert-from-hex-color-to-rgb-struct-in-c
+    return { (unsigned char)((hexadecimalValue >> 16) & 0xFF), (unsigned char)((hexadecimalValue >> 8) & 0xFF), (unsigned char)((hexadecimalValue) & 0xFF) };
 }
 
 void Color::updateGLColor()
--- engine/color/color.h
+++ engine/color/color.h
@@ -7,11 +7,33 @@
 
 namespace Betoneira
 {
+    enum Colors
+    {
+        // http://www.festra.com/cb/art-color.htm
+        AQUA       = 0x00FFFF00,
+        BLACK      = 0x00000000,
+        BLUE       = 0x00FF0000,
+        CREAM      = 0x00F0FBFF,
+        GREY       = 0x00808080,
+        FUCHSIA    = 0x00FF00FF,
+        GREEN      = 0x00008000,
+        LIMEGREEN  = 0x0000FF00,
+        MAROON     = 0x00000080,
+        NAVY       = 0x00800000,
+        OLIVEGREEN = 0x00008080,
+        PURPLE     = 0x00FF00FF,
+        RED        = 0x000000FF,
+        SILVER     = 0x00C0C0C0,
+        TEAL       = 0x00808000,
+        WHITE      = 0x00FFFFFF
+    };
+
     class Color
     {
     public:
         Color();
         Color(unsigned char r, unsigned char g, unsigned char b, unsigned char a=255);
+        Color(int hexadecimal);
         ~Color();
 
         unsigned char getR();
@@ -19,9 +41,13 @@ namespace Betoneira
         unsigned char getB();
         unsigned char getA();
 
-        void setRGBA(unsigned char r, unsigned char g, unsigned char b, unsigned char a=255);
+        void setColor(unsigned char r, unsigned char g, unsigned char b, unsigned char a=255);
+        void setColor(Color newColor);
 
         static GLfloat byteToGLfloat(unsigned char c);
+        
+        // for now, it doesn't support alpha values
+        static Color hexToColor(int hexadecimalValue);
     private:
         unsigned char r = 0, g = 0, b = 0, a = 255;
         GLfloat GLr = 0.0f, GLg = 0.0f, GLb = 0.0f, GLa = 1.0f;
