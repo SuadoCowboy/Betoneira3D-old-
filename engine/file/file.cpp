045c328c1fec1bf6b711e3b0a4a6375c81cb50af
Suado Cowboy <luccacsg@gmail.com>
2024-01-04 23:43:03 -0300

    FileHandler is now functional

--- engine/file/file.cpp
+++ engine/file/file.cpp
@@ -1,24 +1,94 @@
 #include "file.h"
 
-#include <fstream>
-#include <sstream>
-
 using namespace Betoneira;
 
-FileHandler::FileHandler(){};
-FileHandler::~FileHandler(){};
-
-std::string FileHandler::read(std::string path)
+bool FileHandler::pathExists(std::string path)
 {
-    std::ifstream file(path);
-    
-    std::stringstream buffer;
-    buffer << file.rdbuf();
-    
-    return buffer.str();
+    // to read metadata
+    struct stat info;
+
+    if (stat(path.c_str(), &info) == 0)
+        return true;
+    else
+        return false;
+
 }
 
-void FileHandler::write(std::string path, std::string content)
+FileHandler::FileHandler(std::string _path)
 {
+    path = _path;
+};
 
+FileHandler::FileHandler(){}
+FileHandler::~FileHandler(){}
+
+bool FileHandler::isOpen()
+{
+    return file.is_open();
+}
+
+bool FileHandler::open(std::ios_base::openmode mode)
+{
+    if (path.size() == 0)
+    {
+        std::cerr << "ERROR: path could not be opened because size is 0" << std::endl;
+        return true;
+    }
+
+    // creates file if it does not exists
+    if (!pathExists(path))
+    {
+        std::ofstream f(path);
+        f << "";
+        f.close();
+    }
+
+    file.open(path.c_str(), mode);
+
+    return !isOpen();
+}
+
+bool FileHandler::open(std::string newPath, std::ios_base::openmode mode)
+{
+    path = newPath;
+    return open(mode);
+}
+
+bool FileHandler::close()
+{
+    file.close();
+    return isOpen();
+}
+
+bool FileHandler::write(const char* content)
+{
+    if (!isOpen())
+    {
+        std::cerr << "ERROR: file is not open to write to it" << std::endl;
+        return true;
+    }
+
+    file << content;
+
+    return false;
+}
+
+std::string FileHandler::read()
+{
+    if (!isOpen())
+    {
+        std::cerr << "ERROR: file is not open to read it" << std::endl;
+        return "";
+    }
+    
+    std::stringstream buf;
+    buf << file.rdbuf();
+    
+    return buf.str();
+}
+
+FileHandler& operator<<(FileHandler& file, const char* content)
+{
+    file.write(content);
+    return file;
 }
--- engine/file/file.h
+++ engine/file/file.h
@@ -1,15 +1,46 @@
 #pragma once
 
+#include <iostream>
+#include <fstream>
+#include <sstream>
 #include <string>
+#include <sys/stat.h>
 
 namespace Betoneira
 {
     class FileHandler
     {
+    public:
+        std::string path{};
+
+        FileHandler(std::string path);
         FileHandler();
         ~FileHandler();
         
-        virtual std::string read(std::string path);
-        virtual void write(std::string path, std::string content);
-    }
+        // return 1 if error
+        virtual bool open(std::ios_base::openmode mode);
+        
+        // return 1 if error
+        virtual bool open(std::string path, std::ios_base::openmode mode);
+        
+        // return 1 if open
+        virtual inline bool isOpen();
+        
+        // return 1 if error
+        virtual bool close();
+        
+        // return 1 if error
+        virtual bool write(const char* content);
+
+        // should be closed and opened again to read the updated file(if you wrote something to it)
+        virtual std::string read();
+
+        // return 1 if exists
+        static bool pathExists(std::string path);
+
+    private:
+        std::fstream file;
+    };
 }
+
+Betoneira::FileHandler& operator<<(Betoneira::FileHandler& file, const char* content);
