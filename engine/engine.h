b7226c8d51d761e4b52823e59140d7af64983089
Suado Cowboy <luccacsg@gmail.com>
2024-01-10 13:53:31 -0300

    include time/time.h

--- engine/engine.h
+++ engine/engine.h
@@ -12,6 +12,7 @@
 #include "math/math.h" // already includes everything inside math folder
 #include "render/render.h"
 #include "graphics/graphics.h"
+#include "time/time.h"
 
 namespace Betoneira
 {
