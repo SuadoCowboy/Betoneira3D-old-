e72c3660be4019b9ca2b9d68430fa6f1e8f7394f
Suado Cowboy <luccacsg@gmail.com>
2024-01-09 21:20:31 -0300

    created vector2, vector3 and vector4

---
+++ engine/vector/vector.cpp
@@ -0,0 +1,245 @@
+#include "vector.h"
+
+using namespace Betoneira;
+
+// Vector2
+
+template <typename T>
+Vector2<T>::Vector2() : x(0), y(0) {}
+
+template <typename T>
+Vector2<T>::Vector2(T _x, T _y) : x(_x), y(_y){}
+
+template <typename T>
+Vector2<T> Vector2<T>::operator+(const Vector2<T>& other) const {
+    return Vector2<T>(x + other.x, y + other.y);
+}
+
+template <typename T>
+Vector2<T> Vector2<T>::operator-(const Vector2<T>& other) const {
+    return Vector2<T>(x - other.x, y - other.y);
+}
+
+template <typename T>
+Vector2<T> Vector2<T>::operator*(const T& scalar) const {
+    return Vector2<T>(x * scalar, y * scalar);
+}
+
+template <typename T>
+Vector2<T>& Vector2<T>::operator*=(const T& scalar) {
+    x *= scalar;
+    y *= scalar;
+    return *this;
+}
+
+template <typename T>
+Vector2<T> Vector2<T>::operator/(const T& scalar) const {
+    return Vector2<T>(x / scalar, y / scalar);
+}
+
+template <typename T>
+Vector2<T>& Vector2<T>::operator/=(const T& scalar) {
+    x /= scalar;
+    y /= scalar;
+    return *this;
+}
+
+template <typename T>
+Vector2<T>& Vector2<T>::operator+=(const Vector2<T>& other) {
+    x += other.x;
+    y += other.y;
+    return *this;
+}
+
+template <typename T>
+Vector2<T>& Vector2<T>::operator-=(const Vector2<T>& other) {
+    x -= other.x;
+    y -= other.y;
+    return *this;
+}
+
+template <typename T>
+bool Vector2<T>::operator==(const Vector2<T>& other) const {
+    return (x == other.x) && (y == other.y);
+}
+
+template <typename T>
+bool Vector2<T>::operator!=(const Vector2<T>& other) const {
+    return !(*this == other);
+}
+
+// Vector3
+
+template <typename T>
+Vector3<T>::Vector3(){}
+
+template <typename T>
+Vector3<T>::Vector3(T _x, T _y, T _z) : x(_x), y(_y), z(_z){}
+
+template <typename T>
+Vector3<T> Vector3<T>::operator+(const Vector3<T>& other) const
+{
+    return Vector3<T>(x + other.x, y + other.y, z + other.z);
+}
+
+template <typename T>
+Vector3<T> Vector3<T>::operator-(const Vector3<T>& other) const
+{
+    return Vector3<T>(x - other.x, y - other.y, z - other.z);
+}
+
+template <typename T>
+Vector3<T> Vector3<T>::operator*(const T& scalar) const
+{
+    return Vector3<T>(x * scalar, y * scalar, z * scalar);
+}
+
+template <typename T>
+Vector3<T>& Vector3<T>::operator*=(const T& scalar)
+{
+    x *= scalar;
+    y *= scalar;
+    z *= scalar;
+    return *this;
+}
+
+template <typename T>
+Vector3<T> Vector3<T>::operator/(const T& scalar) const
+{
+    return Vector3<T>(x / scalar, y / scalar, z / scalar);
+}
+
+template <typename T>
+Vector3<T>& Vector3<T>::operator/=(const T& scalar)
+{
+    x /= scalar;
+    y /= scalar;
+    z /= scalar;
+    return *this;
+}
+
+template <typename T>
+Vector3<T>& Vector3<T>::operator+=(const Vector3<T>& other)
+{
+    x += other.x;
+    y += other.y;
+    z += other.z;
+    return *this;
+}
+
+template <typename T>
+Vector3<T>& Vector3<T>::operator-=(const Vector3<T>& other)
+{
+    x -= other.x;
+    y -= other.y;
+    z -= other.z;
+    return *this;
+}
+
+template <typename T>
+bool Vector3<T>::operator==(const Vector3<T>& other) const
+{
+    return (x == other.x) && (y == other.y) && (z == other.z);
+}
+
+template <typename T>
+bool Vector3<T>::operator!=(const Vector3<T>& other) const
+{
+    return !(*this == other);
+}
+
+// Vector4
+
+template <typename T>
+Vector4<T>::Vector4(){}
+
+template <typename T>
+Vector4<T>::Vector4(T _x, T _y, T _z, T _w) : x(_x), y(_y), w(_w){}
+
+template <typename T>
+Vector4<T> Vector4<T>::operator+(const Vector4<T>& other) const
+{
+    return Vector4<T>(x + other.x, y + other.y, z + other.z, w + other.w);
+}
+
+template <typename T>
+Vector4<T> Vector4<T>::operator-(const Vector4<T>& other) const
+{
+    return Vector4<T>(x - other.x, y - other.y, z - other.z, w - other.w);
+}
+
+template <typename T>
+Vector4<T> Vector4<T>::operator*(const T& scalar) const
+{
+    return Vector4<T>(x * scalar, y * scalar, z * scalar, w * scalar);
+}
+
+template <typename T>
+Vector4<T>& Vector4<T>::operator*=(const T& scalar)
+{
+    x *= scalar;
+    y *= scalar;
+    z *= scalar;
+    w *= scalar;
+    return *this;
+}
+
+template <typename T>
+Vector4<T> Vector4<T>::operator/(const T& scalar) const
+{
+    return Vector4<T>(x / scalar, y / scalar, z / scalar, w / scalar);
+}
+
+template <typename T>
+Vector4<T>& Vector4<T>::operator/=(const T& scalar)
+{
+    x /= scalar;
+    y /= scalar;
+    z /= scalar;
+    w /= scalar;
+    return *this;
+}
+
+template <typename T>
+Vector4<T>& Vector4<T>::operator+=(const Vector4<T>& other)
+{
+    x += other.x;
+    y += other.y;
+    z += other.z;
+    w += other.w;
+    return *this;
+}
+
+template <typename T>
+Vector4<T>& Vector4<T>::operator-=(const Vector4<T>& other)
+{
+    x -= other.x;
+    y -= other.y;
+    z -= other.z;
+    w -= other.w;
+    return *this;
+}
+
+template <typename T>
+bool Vector4<T>::operator==(const Vector4<T>& other) const
+{
+    return (x == other.x) && (y == other.y) && (z == other.z);
+}
+
+template <typename T>
+bool Vector4<T>::operator!=(const Vector4<T>& other) const
+{
+    return !(*this == other);
+}
+
+template class Vector2<float>;
+template class Vector2<unsigned int>;
+template class Vector2<int>;
+
+template class Vector3<float>;
+template class Vector3<unsigned int>;
+template class Vector3<int>;
+
+template class Vector4<float>;
+template class Vector4<unsigned int>;
+template class Vector4<int>;
---
+++ engine/vector/vector.h
@@ -0,0 +1,113 @@
+#pragma once
+
+#include <string>
+#include <sstream>
+
+namespace Betoneira
+{
+    template <typename T>
+    class Vector2
+    {
+    public:
+        Vector2();
+        Vector2(T x, T y);
+
+        T x;
+        T y;
+
+        // made by ChatGPT xd...
+        Vector2<T> operator+(const Vector2<T>& other) const;
+        Vector2<T> operator-(const Vector2<T>& other) const;
+        Vector2<T> operator*(const T& scalar) const;
+        Vector2<T>& operator*=(const T& scalar);
+        Vector2<T> operator/(const T& scalar) const;
+        Vector2<T>& operator/=(const T& scalar);
+        Vector2<T>& operator+=(const Vector2<T>& other);
+        Vector2<T>& operator-=(const Vector2<T>& other);
+        bool operator==(const Vector2<T>& other) const;
+        bool operator!=(const Vector2<T>& other) const;
+    };
+    
+    template <typename T>
+    class Vector3
+    {
+    public:
+        Vector3();
+        Vector3(T x, T y, T z);
+
+        T x;
+        T y;
+        T z;
+
+        Vector3<T> operator+(const Vector3<T>& other) const;
+        Vector3<T> operator-(const Vector3<T>& other) const;
+        Vector3<T> operator*(const T& scalar) const;
+        Vector3<T>& operator*=(const T& scalar);
+        Vector3<T> operator/(const T& scalar) const;
+        Vector3<T>& operator/=(const T& scalar);
+        Vector3<T>& operator+=(const Vector3<T>& other);
+        Vector3<T>& operator-=(const Vector3<T>& other);
+        bool operator==(const Vector3<T>& other) const;
+        bool operator!=(const Vector3<T>& other) const;
+    };
+
+    template <typename T>
+    class Vector4
+    {
+    public:
+        Vector4();
+        Vector4(T x, T y, T z, T w);
+
+        T x;
+        T y;
+        T z;
+        T w;
+
+        Vector4<T> operator+(const Vector4<T>& other) const;
+        Vector4<T> operator-(const Vector4<T>& other) const;
+        Vector4<T> operator*(const T& scalar) const;
+        Vector4<T>& operator*=(const T& scalar);
+        Vector4<T> operator/(const T& scalar) const;
+        Vector4<T>& operator/=(const T& scalar);
+        Vector4<T>& operator+=(const Vector4<T>& other);
+        Vector4<T>& operator-=(const Vector4<T>& other);
+        bool operator==(const Vector4<T>& other) const;
+        bool operator!=(const Vector4<T>& other) const;
+    };
+    
+    typedef Vector2<int> Vector2i;
+    typedef Vector2<unsigned int> Vector2u;
+    typedef Vector2<float> Vector2f;
+
+    typedef Vector3<int> Vector3i;
+    typedef Vector3<unsigned int> Vector3u;
+    typedef Vector3<float> Vector3f;
+    
+    typedef Vector4<int> Vector4i;
+    typedef Vector4<unsigned int> Vector4u;
+    typedef Vector4<float> Vector4f;
+    
+    template <typename T>
+    std::string vector2ToString(const Vector2<T>& vector)
+    {
+        std::ostringstream stringStream;
+        stringStream << "(" << vector.x << ", " << vector.y << ")";
+        return stringStream.str();
+    }
+
+    template <typename T>
+    std::string vector3ToString(const Vector3<T>& vector)
+    {
+        std::ostringstream stringStream;
+        stringStream << "(" << vector.x << ", " << vector.y << ", " << vector.z << ")";
+        return stringStream.str();
+    }
+
+    template <typename T>
+    std::string vector4ToString(const Vector4<T>& vector)
+    {
+        std::ostringstream stringStream;
+        stringStream << "(" << vector.x << ", " << vector.y << ", " << vector.z << ", " << vector.w << ")";
+        return stringStream.str();
+    }
+}
