3bd205b704e5e2ec937a4a1664073396f317e196
Suado Cowboy <luccacsg@gmail.com>
2024-01-09 21:23:03 -0300

    OUR FIRST TRIANGLE!

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,19 +1,42 @@
 #include "engine.h"
 
+#include <iostream>
+#include <string>
+
 int main(int, char**)
 {
     Betoneira::init();
 
-    Betoneira::Color backgroundColor(Betoneira::Random::randomColor());
+    Betoneira::Window window(800, 600, "Betoneira3D's First Triangle!");
 
-    Betoneira::Window window(800, 600, "2D Game Test");
+    Betoneira::Color backgroundColor(21, 30, 59);
+
+    Betoneira::Shader simpleShader;
+
+    {
+        Betoneira::FileHandler fileHandler{"assets/shaders/simple2D.vert"};
+        fileHandler.open(std::ios_base::in);
+
+        std::string vertexShaderSource = fileHandler.read();
+        
+        fileHandler.open("assets/shaders/simple2D.frag", std::ios_base::in);
+        std::string fragmentShaderSource = fileHandler.read();
+        fileHandler.close();
+        
+        simpleShader.compile(vertexShaderSource.c_str(), fragmentShaderSource.c_str());
+    }
+
+    Betoneira::Graphics2D::Triangle betoneiraFirstTriangle{simpleShader, {-0.5f, -0.5f}, {0.5f, -0.5f}, {0.0f, 0.5f} };
+
     while (!window.shouldClose())
     {
         window.fill(backgroundColor);
 
         Betoneira::Input::update();
 
-        if (Betoneira::Input::keyJustReleased(Betoneira::Input::Keys::Q))
+        betoneiraFirstTriangle.draw(window);
+
+        if (Betoneira::Input::keyJustPressed(Betoneira::Input::Keys::Q))
             backgroundColor.setColor(Betoneira::Random::randomColor());
         
         window.clear();
